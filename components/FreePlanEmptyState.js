b5a547c5bc9f7854c824dca4e1e085d29daaa150
Eric Crescioni <ecam900@gmail.com>
2020-08-12 20:00:48 -0400

    Create dashboard shell (2nd try, didn't work right 1st time).

---
+++ components/FreePlanEmptyState.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Heading, Box, Text, Button } from '@chakra-ui/core';
+import DashboardShell from './DashboardShell';
+
+const FreePlanEmptyState = () => (
+  <DashboardShell>
+    <Box width='100%' backgroundColor='white' borderRadius='8px' p={8}>
+      <Heading size='lg'>Get feedback on your site instantly</Heading>
+      <Text>Start today, then grow with us.</Text>
+      <Button>Upgrade to Starter</Button>
+    </Box>
+  </DashboardShell>
+);
+export default FreePlanEmptyState;
